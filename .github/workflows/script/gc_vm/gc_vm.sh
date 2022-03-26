3dec6195c0bcbee3492b0b1525e2bfde568d7eb0
wjkim <wj89.kim@samsung.com>
2022-03-26 10:52:42 +0900

    fix the name of 72hour_test job in 72 hour workflow and set totalsize_in_gb to 50 for GC_VM job in coverage workflow

--- .github/workflows/script/gc_vm/gc_vm.sh
+++ .github/workflows/script/gc_vm/gc_vm.sh
@@ -1,7 +1,7 @@
 target_fabric_ip=127.0.0.1
 pos_working_dir="$1" #추후 수정
 
-cd ${pos_working_dir}/test/regression/; sudo ./long_term_ci_test.sh -c true
+cd ${pos_working_dir}/test/regression/; sudo ./long_term_ci_test.sh  -s 50 -c true # add size 50 option
 
 if [ $? -eq 0 ];
 then
